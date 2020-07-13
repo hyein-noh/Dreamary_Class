2285bcc132dfc165f1fd0bbcc49a846d15648af8
hyein-noh <nohyein82@gmail.com>
2020-07-13 12:49:22 +0900

    2

--- page/views.py
+++ page/views.py
@@ -4,7 +4,7 @@ from .models import Designer
 # Create your views here.
 def home(request):
     designers = Designer.objects.all()
-    return render(request, 'home.html',{'designers' : 'designers'})
+    return render(request, 'home.html',{'designers' : designers})
 
 def introduce(request):
     return render(request, 'introduce.html')
