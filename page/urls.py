f765c2956b66379e373fcd2cf6f75a4aa12044be
hyein-noh <nohyein82@gmail.com>
2020-07-17 23:12:46 +0900

    new

--- page/urls.py
+++ page/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name = "home"),
     path('introduce/', views.introduce, name = "introduce"),
-    path('profile/<int:designer_id>', views.detail, name="detail"),
+    path('profile/<int:designer_id>/', views.detail, name="detail"),
+    path('new/', views.new, name='new'),
 ]
